d451952e29abc056cbfe3908fee8db319a8516a8
Paul Coffey <pcoffey2@gmail.com>
2018-07-31 12:48:36 +0900

    Added templates, home,about page

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Jack Bauer',
+        'title': 'Blog Post 1',
+        'content': 'POSTY content 1',
+        'date_posted': 'June 20, 2018'
+    },
+    {
+        'author': 'Shelly Bauer',
+        'title': 'Blog Post 20',
+        'content': 'POSTY Mc Post content 5',
+        'date_posted': 'May 20, 2018'
+    }
+]
 
 @app.route('/')
-def hello_world():
-    return '<h1>Hello World!</h1>'
+@app.route('/home')
+def home():
+    return render_template('index.html', posts=posts)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
